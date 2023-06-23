7e8589d2e4d29a3c0686b48371a85ebc3e782aa3
Tycho Pandelaar <tycho.pandelaar@klm.com>
2023-06-23 10:03:12 +0200

    https://stackoverflow.com/questions/58000140/swift-package-manager-storyboard-bundle

--- ProblemDemo/AppDelegate.swift
+++ ProblemDemo/AppDelegate.swift
@@ -10,7 +10,26 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+	override init() {
+		super.init()
 
+		// WORKAROUND: Storyboards do not trigger the loading of resource bundles in Swift Packages.
+		// Taken from https://stackoverflow.com/questions/58000140/swift-package-manager-storyboard-bundle
+		let bundleNames = ["MyLibrary_MyLibrary"]
+		bundleNames.forEach { (bundleName) in
+			guard
+				let bundleURL = Bundle.main.url(forResource: bundleName, withExtension: "bundle"),
+				let bundle = Bundle(url: bundleURL) else {
+				preconditionFailure()
+			}
+			bundle.load()
+		}
+
+		let bundles = Bundle.allBundles
+		bundles.forEach { (bundle) in
+			print("Bundle identifier loaded: \(bundle.bundleIdentifier ?? "Bundle \(bundle) has no identifier")")
+		}
+	}
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 		// Override point for customization after application launch.
@@ -30,7 +49,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 		// If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
 		// Use this method to release any resources that were specific to the discarded scenes, as they will not return.
 	}
-
-
 }
 
