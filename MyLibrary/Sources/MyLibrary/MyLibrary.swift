fec5089d1464ceb0bb00bf3dc3dd672d239a6e72
DonMag <don_mail_2000@yahoo.com>
2023-06-23 08:26:44 -0400

    Maybe?

--- MyLibrary/Sources/MyLibrary/MyLibrary.swift
+++ MyLibrary/Sources/MyLibrary/MyLibrary.swift
@@ -4,3 +4,28 @@ public struct MyLibrary {
     public init() {
     }
 }
+
+import UIKit
+
+class MyLibraryVC: UIViewController {
+	
+	// let's confirm storyboard outlets / actions work
+	
+	@IBOutlet var mainLabel: UILabel!
+	@IBOutlet var counterLabel: UILabel!
+	
+	var iTapCounter: Int = 0
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		mainLabel.text = "Text set in viewDidLoad()"
+		counterLabel.text = "Tap Count: \(iTapCounter)"
+		
+	}
+	
+	@IBAction func gotTap(_ sender: Any?) {
+		iTapCounter += 1
+		counterLabel.text = "Tap Count: \(iTapCounter)"
+	}
+}
